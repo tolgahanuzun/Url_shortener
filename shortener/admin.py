2e372467e47df9864ab26b608bd4a3ef59e0a2b6
tolgahanuzun <mail@tolgahanuzun.com>
2016-09-07 16:58:18 +0300

    admin panel layout fixed

--- shortener/admin.py
+++ shortener/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+
+from .models import *
 
 # Register your models here.
+admin.site.register(LongURL)
+admin.site.register(ShortURL)
+
