641bc284078e297e20c5ac4ecf1df25981c35354
tolgahanuzun <mail@tolgahanuzun.com>
2016-09-08 00:41:53 +0300

    url

--- Profile/urls.py
+++ Profile/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-
+from django.conf.urls.static import static
 urlpatterns = [
 
     url(r'^login/', 'Profile.views.login'),
     url(r'^logout/', 'Profile.views.logout'),
     url(r'^register/', 'Profile.views.register'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- shortener/urls.py
+++ shortener/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns,  include, url
+from django.conf.urls.static import static
 from shortener import views
 
 urlpatterns = [
     url(r'^add_url/', views.add_site),
     url(r'^([a-z]{5})/$', views.open_short_url),
     url(r'^', views.add_site),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
