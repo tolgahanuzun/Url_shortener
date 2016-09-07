223374fe3df71546707af157092d74a0ccacb911
tolgahanuzun <mail@tolgahanuzun.com>
2016-09-07 23:42:55 +0300

    deploy

--- Url_Shortener/urls.py
+++ Url_Shortener/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from shortener.views import open_short_url
 
 import shortener
@@ -8,6 +10,4 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^auth/', include('Profile.urls')),
     url(r'^', include('shortener.urls')),
-
-
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
