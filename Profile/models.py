89a98db3f0649d6c34506a7a6e5289698c5f5c97
tolgahanuzun <mail@tolgahanuzun.com>
2016-09-07 16:56:35 +0300

    adverside select add

--- Profile/models.py
+++ Profile/models.py
@@ -1,5 +1,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+	user = models.OneToOneField(User)
+
+	def __unicode__(self):
+		return "%s" % (self.user)
