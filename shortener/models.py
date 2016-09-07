b8747f84ab9b6eae930448f4e06fefbcf9621c2a
tolgahanuzun <mail@tolgahanuzun.com>
2016-09-07 16:59:23 +0300

    little field error fixed

--- shortener/models.py
+++ shortener/models.py
@@ -1,18 +1,22 @@
 from django.db import models
-#from django.contrib import auth
+from django.contrib.auth.models import User
 from Url_Shortener import settings
+from Profile.models import Profile
+#from django.contrib import auth
+
 
 class LongURL(models.Model):
     name = models.CharField(max_length = 100)
+    ads  = models.CharField(max_length = 100)
 
     def __str__(self):
         return self.name
 
 class ShortURL(models.Model):
     name = models.CharField(max_length = 50)
-    number_of_clicks = models.IntegerField(default=0)
-    link_to_long = models.ForeignKey(LongURL)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=0, null = True)
+    clicks = models.IntegerField(default=0)
+    long_link = models.ForeignKey(LongURL)
+    user = models.ForeignKey(User, default=0, null = True)
     created_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
